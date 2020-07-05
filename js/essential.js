d8527679851956ea15b1e7a0c9fc6deedf823b47
Weilan Yu <isweilanyu@gmail.com>
2020-07-04 22:10:49 -0700

    Add navbar and data filter

---
+++ js/essential.js
@@ -0,0 +1,52 @@
+/* Reference: https://www.w3schools.com/howto/howto_js_portfolio_filter.asp */
+// Display all projects at load up
+filterSelection("all");
+
+function filterSelection(subject) {
+    var columns, i;
+    // select all column objects
+    columns = document.getElementsByClassName("col");
+    if (subject == "all") subject = "";
+    for (i = 0; i < columns.length; i++) {
+        removeClass(columns[i], "show");
+        // if the className is our taget, add className "show"
+        if (columns[i].className.indexOf(subject) > -1) addClass(columns[i], "show");
+    }
+}
+
+// Show filtered elements
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("filterContainer");
+var btns = btnContainer.getElementsByClassName("filter-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
